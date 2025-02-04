08aec64511a8b82a46ee7a942eef06f46921e525
Lee Cheneler <lacheneler@gmail.com>
2025-02-04 22:08:44 +0000

    chore: update tailwind (#7)

--- src/main.tsx
+++ src/main.tsx
@@ -29,7 +29,7 @@ app.get("/routing", (context) => {
 
 app.get(
   "/public/*",
-  useServeFiles({ directory: resolve(Deno.cwd(), "./src/public") }),
+  useServeFiles({ directory: resolve(Deno.cwd(), "./public") }),
 );
 
 Deno.serve(app.build());
