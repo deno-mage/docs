6ef4bedb5195a96d36240bec9f2a36917191f54b
Lee Cheneler <lacheneler@gmail.com>
2025-02-09 00:37:05 +0000

    docs: updated request docs (#20)

--- src/pages/requests.tsx
+++ src/pages/requests.tsx
@@ -4,19 +4,23 @@ import { CodeBlock } from "../components/code-block.tsx";
 import { Breadcrumb, BreadcrumbList } from "../components/breadcrumbs.tsx";
 import { Text } from "../components/text.tsx";
 
-const REQUEST = `app.post("/", async (context) => {
+const REQUEST = `app.use(async (context) => {
   console.log(context.request.method);
-  console.log(context.request.headers.get("Content-Type"));
-  console.log(context.url.searchParams.get("value"));
+  console.log(context.request.url);
   console.log(await context.request.text());
-
-  context.text(StatusCode.OK, "Hello, World!");
 });`;
 
-const URL = `app.get("/", (context) => {
-  console.log(context.url);
-  
-  context.text(StatusCode.OK, "Hello, World!");
+const HEADER = `app.use(async (context) => {
+  console.log(context.request.header("Content-Type"));
+});`;
+
+const SEARCH_PARAM = `app.use(async (context) => {
+  console.log(context.url.searchParam("value"));
+});`;
+
+const RAW = `app.use((context) => {
+  console.log(context.request.raw.method);
+  console.log(context.request.raw.url);
 });`;
 
 export const RequestsPage = () => {
@@ -35,21 +39,37 @@ export const RequestsPage = () => {
           You have full access to the raw request object in all middleware.
         </Text>
 
-        <Heading level={2}>Request</Heading>
+        <Heading level={2}>MageRequest</Heading>
         <Text as="p">
-          The request instance is available at{" "}
-          <Text as="code">context.request</Text>. You can use this to
-          interrogate the incoming request.
+          The request object is available on the context as a{" "}
+          <Text as="code">MageRequest</Text>{" "}
+          that provides memoized access to the body utility methods like{" "}
+          <Text as="code">text()</Text>, <Text as="code">json()</Text>, and{" "}
+          <Text as="code">formData()</Text>.
         </Text>
         <CodeBlock>{REQUEST}</CodeBlock>
 
-        <Heading level={2}>URL</Heading>
+        <Heading level={2}>Headers</Heading>
         <Text as="p">
-          For convenience a URL instance is available at{" "}
-          <Text as="code">context.url</Text>. This is a parsed representation of
-          the request URL.
+          You can read header values from the request object using the{" "}
+          <Text as="code">header()</Text> method.
         </Text>
-        <CodeBlock>{URL}</CodeBlock>
+        <CodeBlock>{HEADER}</CodeBlock>
+
+        <Heading level={2}>Search Params</Heading>
+        <Text as="p">
+          You can read search parameters from the request object using the{" "}
+          <Text as="code">searchParam()</Text> method.
+        </Text>
+        <CodeBlock>{SEARCH_PARAM}</CodeBlock>
+
+        <Heading level={2}>Raw Request</Heading>
+        <Text as="p">
+          The raw request object is available on the context as{" "}
+          <Text as="code">context.request.raw</Text>{" "}
+          and provides access to the raw request object.
+        </Text>
+        <CodeBlock>{RAW}</CodeBlock>
       </div>
     </Page>
   );
