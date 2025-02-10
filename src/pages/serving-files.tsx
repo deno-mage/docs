af47f37ed0a960ec4e5604a02ea33e00cdd2e1c4
Lee Cheneler <lacheneler@gmail.com>
2025-02-10 23:58:20 +0000

    docs: cache busting files (#24)

--- src/pages/serving-files.tsx
+++ src/pages/serving-files.tsx
@@ -19,6 +19,22 @@ const NO_INDEX_HTML = `app.get(
   }),
 );`;
 
+const CACHE_BUSTING = `app.get("/", (context) => {
+  await context.render(
+    StatusCode.OK,
+    <html lang="en">
+      <body>
+        <img src={context.asset("/public/image.png")} />
+      </body>
+    </html>,
+  );
+});
+
+app.get(
+  "/public/*",
+  useServeFiles({ directory: resolve(Deno.cwd(), "./public") })
+);`;
+
 export const ServingFilesPage = () => {
   return (
     <Page description="Learn about Mage" title="Serving files">
@@ -56,6 +72,26 @@ export const ServingFilesPage = () => {
           option to <Text as="code">false</Text>.
         </Text>
         <CodeBlock>{NO_INDEX_HTML}</CodeBlock>
+
+        <Heading level={3}>Serving files with cache busting</Heading>
+        <Text as="p">
+          Files can be referenced with the build id appended to the URL in the
+          format{" "}
+          <Text as="code">{`/public/styles.css.{BUILD_ID}`}</Text>. When this is
+          done, Mage will apply a long cache header to the file.
+        </Text>
+        <Text as="p">
+          This is useful for cache busting and ensuring that the latest version
+          of the file is always served. For instance CSS, JavaScript and image
+          files.
+        </Text>
+        <Text as="p">
+          The utility method <Text as="code">context.asset()</Text>{" "}
+          can be used to generate the URL with the build id appended. The
+          following example demonstrates how to use this method to serve an
+          image file rendered in HTML.
+        </Text>
+        <CodeBlock>{CACHE_BUSTING}</CodeBlock>
       </div>
     </Page>
   );
